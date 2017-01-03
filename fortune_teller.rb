63518d6735d6e9b908a5390c4877f788086acec1
ginkouno <kouno.ruby@gmail.com>
2017-01-03 16:12:02 +0900

    Extract code for trello to new class

--- fortune_teller.rb
+++ fortune_teller.rb
@@ -1,19 +1,16 @@
 require 'slack-ruby-bot'
 require 'yaml'
 require 'dotenv'
-require 'trello'
-
-Dotenv.load
-Trello.configure do |config|
-  config.consumer_key    = ENV['TRELLO_KEY']
-  config.consumer_secret = ENV['TRELLO_SECRET']
-  config.oauth_token     = ENV['TRELLO_OAUTH_TOKEN']
-end
-# me = Trello::Member.find("me")
+require './sample_trello'
 
 class FortuneTeller < SlackRubyBot::Bot
+  Dotenv.load
+
   command 'please' do |client, data, match|
     yaml = YAML.load_file('lucky_comments.yml')
+    trello = SampleTrello.new
+    result = trello.sample
+    client.say(text: "#{result[:list_name]} #{result[:card_name]}", channel: data.channel)
     client.say(text: yaml['comments'].sample, channel: data.channel)
   end
 end
---
+++ sample_trello.rb
@@ -0,0 +1,25 @@
+require 'dotenv'
+require 'trello'
+
+class SampleTrello
+  def initialize
+    Dotenv.load
+    Trello.configure do |config|
+      config.consumer_key    = ENV['TRELLO_KEY']
+      config.consumer_secret = ENV['TRELLO_SECRET']
+      config.oauth_token     = ENV['TRELLO_OAUTH_TOKEN']
+    end
+  end
+
+  def settings
+    @settings ||= YAML.load_file('trello.yml')
+  end
+
+  def sample
+    me = Trello::Member.find("me")
+    board = me.boards.find{ |b| b.name == settings['board_name'] }
+    lists = board.lists.select{ |list| settings['list_names'].include?(list.name) && list.cards.count > 1 }
+    list = lists.sample
+    { list_name: list.name, card_name: list.cards.first.name }
+  end
+end
